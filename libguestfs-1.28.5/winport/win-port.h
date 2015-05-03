0466c79b068860d06258256ae30eb21cc49930c9
novokrest <novokrest013@gmail.com>
2015-05-03 12:13:40 +0300

    winport: fix some warnings

--- libguestfs-1.28.5/winport/win-port.h
+++ libguestfs-1.28.5/winport/win-port.h
@@ -8,8 +8,8 @@
 #include "win-defs.h"
 
 /* win-vaprintf.c */
-extern int asprintf (char** __restrict ret, const char * __restrict format, ...);
-extern int vasprintf (char** __restrict ret, const char * __restrict format, va_list ap);
+extern int asprintf (char** __restrict ret, const char * __restrict format, ...) __attribute__((format(ms_printf, 2, 3)));
+extern int vasprintf (char** __restrict ret, const char * __restrict format, va_list ap) __attribute__((format(ms_printf, 2, 0)));
 
 /* win-symlink.c */
 extern int symlink (const char *path1, const char *path2);
@@ -25,10 +25,10 @@ extern void* memmem (const void *l, size_t l_len, const void *s, size_t s_len);
 extern FILE *open_memstream (char **ptr, size_t *sizeloc);
 
 /* win-getuid.c */
-extern char *getusername ();
-extern char *getusid ();
-extern int getuid ();
-extern int geteuid ();
+extern char *getusername (void);
+extern char *getusid (void);
+extern int getuid (void);
+extern int geteuid (void);
 
 /* win-error.h */
 extern int strerror_r (int errnum, char *buf, size_t buflen);
